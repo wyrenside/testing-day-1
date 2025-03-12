80bd28a9cdf0b18c062d14f86082b30f0b42224d
wyrenside <haciyev004@icloud.com>
2025-03-12 23:25:23 +0400

    first task

---
+++ task16/task 16/Program.cs
@@ -0,0 +1,14 @@
+﻿#region
+int a = int.Parse(Console.ReadLine());
+int max = 0;
+while (a > 0)
+{
+    if (max < a % 10)
+    {
+        max = a % 10;
+    }
+    a /= 10;
+}
+Console.WriteLine(max);
+#endregion
+
